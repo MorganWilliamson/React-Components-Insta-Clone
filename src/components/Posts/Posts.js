05fa2fa3192bb2278713a5dfb8b0b152811e423d
MorganWilliamson <mwilliamson7695@gmail.com>
2020-10-06 15:29:46 -0500

    Reordered Posts and SearchBar so that they render in the correct spots.

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -4,13 +4,15 @@ import './Posts.css';
 
 const Posts = (props) => {
   // 🔥 Make sure the parent of Posts is passing the right props! //originally passing likePost, posts
-  const { posts, likePosts } = props;
+  const { likePost, posts } = props;
 
   return (
     <div className='posts-container-wrapper'>
       {/* Map through the posts array returning a Post component at each iteration */}
       {/* Check the implementation of Post to see what props it requires! */}
-
+      {posts.map((postObj) => {
+        return <Post key={postObj.id} post={postObj} likePost={likePost} />
+      })}
     </div>
   );
 };
