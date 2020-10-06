7e2470caa561a167f2078fc97d62b98e626810fb
MorganWilliamson <mwilliamson7695@gmail.com>
2020-10-06 14:50:53 -0500

    Added setPosts function to App.js.

--- src/App.js
+++ src/App.js
@@ -17,10 +17,12 @@ const App = () => {
   // Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
+  const [posts, setPosts] = useState(dummyData);
+  const [search, setSearch] = useState('');
 
   const likePost = postId => {
     /*
-      This function serves the purpose of increasing the number of likes by one, of the post with a given id.
+      This function serves the purpose of increasing the number of likes by one, of the post with a given id. //increase by one, so count + 1? 
 
       The state of the app lives at the top of the React tree, but it wouldn't be fair for nested components not to be able to change state!
       This function is passed down to nested components through props, allowing them to increase the number of likes of a given post.
@@ -30,6 +32,14 @@ const App = () => {
         - if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
         - otherwise just return the post object unchanged.
      */
+    setPosts(posts.map((post) => {
+      if (post.id === postId){
+        return {...post, likes: post.likes + 1}
+      }
+      return post;
+    }
+
+    ))
   };
 
   return (
